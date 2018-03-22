93c373e31c95e5a94e00c7cbe7374c05f5420825
Hussein <tejanahmedhu@gmail.com>
2018-03-22 07:56:58 +0000

    assigned POST method to each column in database

--- userInfo.php
+++ userInfo.php
@@ -1,3 +1,12 @@
 <?php
+// enables user to connect to database
+include_once('dbConnect.php');
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$gender = $_POST['gender'];
+$happy_to_receive = $_POST['happy_to_receive'];
+$password = $_POST['password'];
+
 
 ?>
